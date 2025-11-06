c35e802db8596645895abe65551560630e8e40ef
Tran Van Hoang <hoangtvh73@gmail.com>
2025-11-06 10:44:35 +0700

    1 commit

---
+++ src/main/java/com/example/myprojectfa25/MyProjectFA25.java
@@ -0,0 +1,15 @@
+
+
+package com.example.myprojectfa25;
+
+/**
+ *
+ * @author Admin
+ */
+public class MyProjectFA25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
